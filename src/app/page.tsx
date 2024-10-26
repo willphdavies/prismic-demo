63b28a9eba3e9c1d6d69783697ba96398a8988e9
willphdavies <willphdavies@gmail.com>
2024-10-26 11:06:45 -0700

    prismic init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="">
       <main className="">
-        <h1 className="font-display text-5xl">Hello World</h1>
+        <h1 className="font-body text-5xl">Hello World</h1>
       </main>
     </div>
   );
